186fb93139dea0ed8eb5dec57ad2b605f62f874c
Asadbek <95742736+AsadbekUrinov@users.noreply.github.com>
2023-06-26 11:58:48 +0500

    Add files via upload

---
+++ config/locale.php
@@ -0,0 +1,14 @@
+<?php
+
+return [ 
+    'uz' => [
+        'code' => 'uz',
+        'display' => 'Uzbek',
+        'flag' => 'uz'
+    ], 
+    'en' => [
+        'code' => 'en',
+        'display' => 'English',
+        'flag' => 'us'
+    ],
+];
